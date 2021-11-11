b69564ecd35de9222f86941ce62f817cb9782e30
NelsonBeard <ilnur111111yandex.ru>
2021-11-11 22:20:32 +0500

    Добавил navigation drawer и его заголовок

--- app/src/main/java/com/geekbrains/notes/MainActivity.java
+++ app/src/main/java/com/geekbrains/notes/MainActivity.java
@@ -1,11 +1,13 @@
 package com.geekbrains.notes;
 
 import android.os.Bundle;
-import android.view.Gravity;
-import android.view.View;
+import android.view.MenuItem;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,29 +18,37 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_info:
+                        Toast.makeText(MainActivity.this, "Информация о приложении", Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.action_settings:
+                        Toast.makeText(MainActivity.this, "Настройки", Toast.LENGTH_SHORT).show();
+                        return true;
+                }
+                return false;
+            }
+        });
+
         Notes note_1 = new Notes("Заголовок заметки 1", "содержание заметки 1", "01.11.2021");
         Notes note_2 = new Notes("Заголовок заметки 2", "содержание заметки 2", "02.11.2021");
         Notes note_3 = new Notes("Заголовок заметки 3", "содержание заметки 3", "03.11.2021");
         Notes note_4 = new Notes("Заголовок заметки 4", "содержание заметки 4", "04.11.2021");
         notes = new Notes[]{note_1, note_2, note_3, note_4};
 
-        findViewById(R.id.buttonNotesList).setOnClickListener(v -> {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .addToBackStack("")
+                .replace(R.id.notesListFragment_container, new NotesListFragment())
+                .commit();
 
-            getSupportFragmentManager()
-                    .beginTransaction()
-                    .addToBackStack("")
-                    .replace(R.id.notesListFragment_container, new NotesListFragment())
-                    .commit();
-        });
-        findViewById(R.id.buttonSettings).setOnClickListener(v -> {
-            Toast toast = Toast.makeText(this, "Какие-то настройки",Toast.LENGTH_SHORT);
-            toast.show();
-        });
-        findViewById(R.id.buttonInfo).setOnClickListener(v -> {
-            Toast toast = Toast.makeText(this, "Информация о приложении",Toast.LENGTH_SHORT);
-            toast.show();
-        });
+
     }
+
     @Override
     public void onBackPressed() {
         if (getSupportFragmentManager().getFragments().size() > 0) {
@@ -47,4 +57,5 @@ public class MainActivity extends AppCompatActivity {
             super.onBackPressed();
         }
     }
+
 }
